9548cd4a55f4b4697e7717e8e59c5ec63bfd0af9
Clément Dudouyt <cldudouyt@live.fr>
2019-01-25 17:34:54 +0100

    gctu

---
+++ GestionNotes_IHM/Controllers/API/DataController.cs
@@ -0,0 +1,30 @@
+﻿using GestionNotes_DAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Mvc.HttpGetAttribute;
+
+namespace GestionNotes_IHM.Controllers.API
+{
+    public class DataController : ApiController
+    {
+        
+        [HttpGet]
+        public float GetMoyenne(long id)
+        {
+            float moyenne;
+            GestionNotesContext ctx;
+
+            ctx = new GestionNotesContext();
+            moyenne = ctx.Evaluations.Where(o => o.IdEleve == id).Select(o => o.Note).DefaultIfEmpty(0).Average();
+
+           // moyenne = (from i in ctx.Evaluations where o.IdEleve == id select o.Notes).DedaultIdEmpty(0).Average();
+
+            return moyenne;
+        }
+    }
+}
